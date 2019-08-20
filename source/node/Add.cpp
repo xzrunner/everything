a7f9eecee52036d79f94a5c420efdf1d8b9cfd7e
zz <zhuguang@ejoy.com>
2019-08-21 07:49:49 +0800

    [FIXED] add has no input geo

--- source/node/Add.cpp
+++ source/node/Add.cpp
@@ -10,22 +10,23 @@ void Add::Execute(TreeContext& ctx)
 {
     m_geo.reset();
 
+    std::vector<sm::vec3> vertices;
+
     auto prev_geo = GetInputGeo(0);
-    if (!prev_geo) {
-        return;
+    if (prev_geo)
+    {
+        auto& prev_points = prev_geo->GetAttr().GetPoints();
+        vertices.reserve(prev_points.size());
+        for (auto& p : prev_points) {
+            vertices.push_back(p->pos);
+        }
     }
 
-    std::vector<sm::vec3> vertices;
-    auto& prev_points = prev_geo->GetAttr().GetPoints();
-    vertices.reserve(prev_points.size());
-    for (auto& p : prev_points) {
-        vertices.push_back(p->pos);
-    }
     if (vertices.empty() && m_points.empty()) {
         return;
     }
-
     std::copy(m_points.begin(), m_points.end(), std::back_inserter(vertices));
+
     m_geo = std::make_shared<Geometry>(GeoPolyline(vertices));
 }
 
